7419c8de1cf28eceedc68f6a143b294f8cd41086
Wenjie Wei <wenjieweizb@gmail.com>
2018-09-20 23:46:06 -0400

    added .gitignore. L and L^T are now obtained.

--- a1/choleski.py
+++ a1/choleski.py
@@ -1,7 +1,49 @@
 import math
-from matrix import matrix_2d
+from matrix import matrix
 
 
-class choleski(object):
-    def __init__(self):
-        pass
+def choleski_decomp(A, b):
+    """
+    This is the main class implemented for solving the problem Ax = b,
+    using Cholesky Decomposition.
+
+    Arguments:
+        A: A real, S.P.D. (Symmetric positive definite) n * n matrix.
+        b: Column vector with n rows.
+
+    Returns:
+        Column vector x with n rows.
+    """
+    if not A.is_symmetric():
+        raise ValueError("Matrix must be symmetric to perform Choleski Decomposition.\n")
+
+    n = A.rows
+    sparse_matrix = [[0 for _ in range(n)] for _ in range(n)]
+    L = matrix(sparse_matrix, n, n)
+
+    for j in range(n):
+        if A[j][j] < 0:
+            raise ValueError("Matrix is not positive definite.\n")
+
+        temp_sum = 0
+        for k in range(-1, j):
+            temp_sum += math.pow(L[j][k], 2)
+        if (A[j][j] - temp_sum) < 0:
+            raise ValueError("Operand in sqrt must be positive at j = %d." % j)
+        L[j][j] = math.sqrt(A[j][j] - temp_sum)
+        temp_sum = 0
+        for i in range(j+1, n):
+            for k in range(-1, j-1):
+                temp_sum += L[i][k] * L[j][k]
+            L[i][j] = (A[i][j] - temp_sum) / L[j][j]
+
+
+
+if __name__ == "__main__":
+    a = [[15.0, -5.0, 0.0, -5.0], [-5.0, 12.0, -2.0, 0.0], [0.0, -2.0, 6.0, -2.0], [-5.0, 0.0, -2.0, 9.0]]
+    b = [[1, 2, 3]]
+    A = matrix(a, 4, 4)
+    A.print_matrix_property()
+    choleski_decomp(A, b)
+
+    #matrix.print_matrix_property()
--- a1/matrix.py
+++ a1/matrix.py
@@ -11,22 +11,79 @@ class matrix(object):
         return self._rows == self._cols
 
     def is_symmetric(self):
-        return self == self.T
+        if not self.is_square():
+            return False
+
+        else:
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    if self[i][j] != self.T[i][j]:
+                        return False
+
+        return True
 
     def transpose(self):
-        vec_trans = [[None for _ in range(self.cols)] for _ in range(self.rows)]
+        vec_trans = [[None for _ in range(self.rows)] for _ in range(self.cols)]
         for x in range(self.cols):
             for y in range(self.rows):
-                vec_trans[x, y] = self.vec[y, x]
+                vec_trans[x][y] = self.vec[y][x]
 
         transposed_matrix = matrix(vec_trans, self.cols, self.rows)
         return transposed_matrix
 
+    def dot_product(self, other):
+        if self.cols != other.rows:
+            raise ValueError("Incorrect dimension for vector multiplication.")
+
+        result = 0
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result += (self.vec[i][j] * other.vec[j][i])
+
+        return result
+
+    def is_positive_definite(self):
+        pass
+
+    def __getitem__(self, item_number):
+        if isinstance(item_number, int):
+            return self._vec[item_number]
+
+        if isinstance(item_number, tuple):
+            x, y = item_number
+            # use some "dummy entries" as a buffer to decrease the possibility of occurring out of boundary.
+            if x < 0 or x >= self.rows or y < 0 or y >= self.cols:
+                return 0
+            else:
+                return self._vec[x][y]
+
     def clone(self):
         cloned_matrix = matrix(self.vec, self.rows, self.cols)
-
         return cloned_matrix
 
+    def print_matrix_property(self):
+        print("The entered matrix is:\n")
+        for i in range(self.rows):
+            for j in range(self.cols):
+                print("%d" % (self[i][j]), end=" ")
+            print("\n")
+
+        print("The transpose of the entered matrix is:\n")
+        other = self.T
+        for i in range(other.rows):
+            for j in range(other.cols):
+                print("%d" % (other[i][j]), end=" ")
+            print("\n")
+
+        if self.is_square():
+            print("The entered matrix is square.\n")
+        else:
+            print("The entered matrix is not square.\n")
+
+        if self.is_symmetric():
+            print("The entered matrix is symmetric.\n")
+        else:
+            print("The entered matrix is not symmetric.\n")
 
     @property
     def vec(self):
