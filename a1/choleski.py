d2064978d597f7fdc34424838084fd54d0ad3756
Wenjie Wei <wenjieweizb@gmail.com>
2018-09-30 11:34:40 -0400

    Forward elmination bringing the correct answer. Backward substitution seems to be able to get the answer correct, but the precision is too bad. need to find solution.

--- a1/choleski.py
+++ a1/choleski.py
@@ -2,9 +2,9 @@ import math
 from matrix import matrix
 
 
-def choleski_decomp(A, b):
+def choleski_decomposition(A, b):
     """
-    This is the main class implemented for solving the problem Ax = b,
+    This is the method implemented for solving the problem Ax = b,
     using Choleski Decomposition.
 
     Arguments:
@@ -33,46 +33,48 @@ def choleski_decomp(A, b):
         L[j][j] = math.sqrt(A[j][j] - temp_sum)
 
         temp_sum = 0
-        for i in range(j+1, n):
+        for i in range(j + 1, n):
             for k in range(-1, j):
                 temp_sum += L[i][k] * L[j][k]
             L[i][j] = (A[i][j] - temp_sum) / L[j][j]
-    #L.print_matrix_property()
+    L.print_matrix_property()
     # Now L and L transpose are all obtained, we can move to forward elmination
 
-    y_vec = [[None for _ in range(1)]for _ in range(n)]
+    y_vec = [[None for _ in range(1)] for _ in range(n)]
     y = matrix(y_vec, n, 1)
     for i in range(y.rows):
         temp_sum = 0
         if i > 0:
-            for j in range(i-1):
+            for j in range(i):
                 temp_sum += L[i][j] * y[j][0]
             y[i][0] = (b[i][0] - temp_sum) / L[i][i]
         else:
             y[i][0] = b[i][0] / L[i][i]
+
+    # y.print_matrix_property()
     # Now perform back substitution to find x.
-    x_vec = [[0 for _ in range(1)]for _ in range(n)]
+
+    x_vec = [[0 for _ in range(1)] for _ in range(n)]
     x = matrix(x_vec, n, 1)
 
-    for i in range(n-1, 0, -1):
+    for i in range(n - 1, -1, -1):
         temp_sum = 0
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             temp_sum += L[j][i] * x[j][0]
-        x[i][0] = (y[i][0] - temp_sum)/L[i][i]
+        x[i][0] = (y[i][0] - temp_sum) / L[i][i]
 
     return x
 
 
 if __name__ == "__main__":
-    a_vec = [[15.0, -5.0, 0.0, -5.0], [-5.0, 12.0, -2.0, 0.0], [0.0, -2.0, 6.0, -2.0], [-5.0, 0.0, -2.0, 9.0]]
+    a_vec = [[15, -5, 0, -5], [-5, 12, -2, 0], [0, -2, 6, -2], [-5, 0, -2, 9]]
     b_vec = [[115], [22], [-51], [13]]
 
     # test matrix x = [12; 6; -5; 7]
     A = matrix(a_vec, 4, 4)
     b = matrix(b_vec, 4, 1)
-    A.print_matrix_property()
-    x = choleski_decomp(A, b)
+    # A.print_matrix_property()
+    x = choleski_decomposition(A, b)
     x.print_matrix_property()
 
-
-    #matrix.print_matrix_property()
+    # matrix.print_matrix_property()
