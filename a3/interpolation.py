bce45ec9e491c6086459783cac0413bcaf4be59a
Wenjie Wei <wenjieweizb@gmail.com>
2018-11-18 17:26:55 -0500

    a3 test

--- a3/interpolation.py
+++ a3/interpolation.py
@@ -1,1 +1,11 @@
-print("hello world")
+from matrix import Matrix
+import matplotlib.pyplot as plt
+
+
+def lagrange_interpolate(x, y):
+    pass
+
+if __name__ == "__main__":    
+    b_data = [[0, 1.3, 1.4, 1.7, 1.8, 1.9]]
+    b_matrix = Matrix(b_data, 1, 6).T
+    b_matrix.print_matrix()
